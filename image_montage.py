089a01cb0947619e215d0f40da8074021da57804
Becca <bemiller627@gmail.com>
2016-10-22 15:34:01 -0400

    Changed to SURF features

--- image_montage.py
+++ image_montage.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import os
-#import PIL
 
 class image_montage(object):
     
@@ -20,45 +19,52 @@ class image_montage(object):
     
     #Read first image from image set    
     I = image_list[0]
-    
+
     #Initialize features for first image
     gray_image = I[:,:,1]
     gray_image = cv2.equalizeHist(gray_image);
     
     #Detect Harris features. May need to adjust parameters.
-    harrisDetector = cv2.FeatureDetector_create("HARRIS")
-    freakDescriptorExtractor = cv2.DescriptorExtractor_create("FREAK")
-    keypoints = harrisDetector.detect(gray_image)
-    (keypoints, descriptors) = freakDescriptorExtractor.compute(gray_image,keypoints)
-    
+    surf = cv2.SURF();
+    #harrisDetector = cv2.FeatureDetector_create('HARRIS')
+    freakExtractor = cv2.DescriptorExtractor_create('FREAK')
+    keypoints = surf.detect(gray_image)
+    print 3
+    (points, descriptors) = freakExtractor.compute(gray_image,keypoints)
+    print 4
+
     #Initialize transforms to identity matrix
-    tforms = [cv2.getPerspectiveTransform(np.identity(3)) for i in range(len(image_list))]
+    tforms = [np.identity(3) for i in range(len(image_list))]
 
     #Iterate over remaining image pairs
     for index in range(1,len(image_list)):
-        keypoints_previous = keypoints;
+        print 5
+        points_previous = points;
         descriptors_previous = descriptors;
         I = image_list[index]
         gray_image = I[:,:,1]
-        gray_image = cv.equalizeHist(gray_image)
+        gray_image = cv2.equalizeHist(gray_image)
         
         #Detect Harris features. May need to adjust parameters.
-        harrisDetector = cv2.FeatureDetector_create("HARRIS")
-        freakDescriptorExtractor = cv2.DescriptorExtractor_create("FREAK")
-        keypoints = harrisDetector.detect(gray_image)
-        (keypoints, descriptors) = freakDescriptorExtractor.compute(gray_image,keypoints)
-        
+        surf = cv2.SURF();
+        #harrisDetector = cv2.FeatureDetector_create('HARRIS')
+        freakExtractor = cv2.DescriptorExtractor_create('FREAK')
+        keypoints = surf.detect(gray_image)
+        (points, descriptors) = freakExtractor.compute(gray_image,keypoints)
+
         #Match descriptors
         bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-        matches = bf.match(des1,des2)
-        matchedPoints = matches[2:3, :];
-        matchedPointsPrevious = matches[0:1, :];
-        print 3
+        matches = bf.match(descriptors,descriptors_previous)
+        #These two lines are probably definitely wrong:
+        #matchedPoints = matches[2:3, :];
+        #matchedPointsPrev = matches[0:1, :];
         
-        # Apply ratio test
-        #good = []
-        #for m,n in matches:
-        #    if m.distance < 0.75*n.distance:
-        #        good.append([m])
+        #tforms[n] = getPerspectiveTransform(matchedPoints, matchedPointsPrev)
+        #tforms[n] = tforms[n-1] * tforms[n]
+    print 6
+   
+ #  for index in range(len(tforms)):
+  #     [xlim[i,:], ylim[i,:]] = outputLimits(tforms(i), [1 imageSize(2)], [1 imageSize(1)]) 
 
+    
         
