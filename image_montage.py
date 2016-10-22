5597558f130fb518c7c604ce9f93ff8dfefd6dff
Becca <bemiller627@gmail.com>
2016-10-22 12:08:44 -0400

    Continued updating.

--- image_montage.py
+++ image_montage.py
@@ -4,6 +4,11 @@ import os
 #import PIL
 
 class image_montage(object):
+    
+    max_corners = 50
+    quality = 0.01
+    min_distance = 5
+    
     #Load images
     image_list = []
     count = 0;
@@ -20,4 +25,10 @@ class image_montage(object):
     gray_image = I[:,:,1]
     gray_image = cv2.equalizeHist(gray_image);
     #Detect Harris features. May need to adjust parameters.
-    points = cv2.cornerHarris(gray_image,2,3,0.04) 
+    points = cv2.goodFeaturesToTrack(gray_image,max_corners,quality,min_distance,useHarrisDetector = 1)
+        #keypoints = cv2.cornerHarris(gray_image,2,3,0.04) 
+    
+    #Convert points to cv::KeyPoint
+    
+    freakExtractor = cv2.DescriptorExtractor_create('FREAK')
+    points,descriptors = freakExtractor.compute(gray_image,points)
