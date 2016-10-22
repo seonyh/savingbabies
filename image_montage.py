e264379601d681066474bf1098e09ca76374ce5d
Becca <bemiller627@gmail.com>
2016-10-22 12:59:07 -0400

    Began feature matching.

--- image_montage.py
+++ image_montage.py
@@ -31,5 +31,22 @@ class image_montage(object):
     keypoints = harrisDetector.detect(gray_image)
     (keypoints, descriptors) = freakDescriptorExtractor.compute(gray_image,keypoints)
     
+    #Initialize transforms to identity matrix
+    tforms = [cv2.warpPerspective(np.identity(3)) for i in range(len(image_list))]
+
+    #Iterate over remaining image pairs
+    for index in range(1,len(image_list)):
+        keypoints_previous = keypoints;
+        descriptors_previous = descriptors;
+        I = image_list[index]
+        gray_image = I[:,:,1]
+        gray_image = cv.equalizeHist(gray_image)
+        
+        #Detect Harris features. May need to adjust parameters.
+        harrisDetector = cv2.FeatureDetector_create("HARRIS")
+        freakDescriptorExtractor = cv2.DescriptorExtractor_create("FREAK")
+        keypoints = harrisDetector.detect(gray_image)
+        (keypoints, descriptors) = freakDescriptorExtractor.compute(gray_image,keypoints)
+        
     
     
